98faa8c33cb3410b8c39400d3bce77ba400ca21d
Will Eastcott <will@playcanvas.com>
2025-01-29 16:57:41 +0000

    Add support for context attributes

--- src/canvas-rendering-context-2d.mjs
+++ src/canvas-rendering-context-2d.mjs
@@ -48,11 +48,29 @@ class CanvasRenderingContext2D {
     /** @type {HTMLCanvasElement} */
     canvas;
 
+    /** @type {Object} */
+    #contextAttributes;
+
     /**
      * @param {HTMLCanvasElement} canvas - The canvas element to create the context from.
+     * @param {Object} [contextAttributes] - Optional attributes for the context.
      */
-    constructor(canvas) {
+    constructor(canvas, contextAttributes = {}) {
         this.canvas = canvas;
+        this.#contextAttributes = {
+            alpha: true,
+            colorSpace: 'srgb',
+            desynchronized: false,
+            willReadFrequently: false,
+            ...contextAttributes
+        };
+    }
+
+    /**
+     * @returns {Object} The context attributes.
+     */
+    getContextAttributes() {
+        return { ...this.#contextAttributes };
     }
 }
 
--- src/html-canvas-element.mjs
+++ src/html-canvas-element.mjs
@@ -5,14 +5,15 @@ import { WebGLRenderingContext } from './webgl-rendering-context.mjs';
 class HTMLCanvasElement extends HTMLElement {
     /**
      * @param {string} contextType - The type of context to create.
+     * @param {Object} [contextAttributes] - Optional attributes for the context.
      * @returns {CanvasRenderingContext2D|WebGLRenderingContext|null} The requested context.
      */
-    getContext(contextType) {
+    getContext(contextType, contextAttributes = {}) {
         switch (contextType) {
             case '2d':
-                return new CanvasRenderingContext2D(this);
+                return new CanvasRenderingContext2D(this, contextAttributes);
             case 'webgl':
-                return new WebGLRenderingContext(this);
+                return new WebGLRenderingContext(this, contextAttributes);
         }
         return null;
     }
